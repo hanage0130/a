27a2efa128cf36e62d4b8a03ebc02e60775e04eb
hanage0130 <60529832+hanage0130@users.noreply.github.com>
2026-09-19 14:38:04 +0900

    wip

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const container = document.querySelector(".container");
 
-let word = "abcdefghijklmnopqrstuvwxyz";
+let str = "";
+let word = document.createElement("div");
+const words = ["correction","harmony","admission","equal","talkative","twin","camp","middle","candidate","facade","acid","bronze","reinforce","tire","sugar","fragrant","ballot","basic","pocket","vigorous","swear","text","coin","copper","rack","small","pain","nest","spit","sea","residence","deter","soar","win","or","differ","stay","direct","hour","old","by","admire","presentation","artificial","thesis","spite","hospital","project","recruit","discount"]
+;
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+for (let i = 0; i < 10; i++) {
+  str += getRandomWord();
+  str += " ";
+}
+
+word.classList.add("word");
 
 function updateDisplay() {
-  container.textContent = word;
+  word.textContent = str;
+  container.textContent = "";
+  container.appendChild(word);
 };
 
 document.addEventListener('keydown', (event) => {
-  if (event.key === word[0]) {
-    word = word.slice(1, word.length)
+  if (event.key === str[0]) {
+    str = str.slice(1, str.length)
+    if (event.key === " ") {
+      str += getRandomWord();
+      str += " ";
+    }
   }
   updateDisplay();
 });
