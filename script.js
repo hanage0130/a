cfdbc910a00b78f75d7e24129031159e4b39cfcc
hanage0130 <60529832+hanage0130@users.noreply.github.com>
2026-09-19 16:36:29 +0900

    add endless mode, display time and wpm

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const container = document.querySelector(".container");
 
 let str = "";
 let word = document.createElement("div");
-const words = ["correction","harmony","admission","equal","talkative","twin","camp","middle","candidate","facade","acid","bronze","reinforce","tire","sugar","fragrant","ballot","basic","pocket","vigorous","swear","text","coin","copper","rack","small","pain","nest","spit","sea","residence","deter","soar","win","or","differ","stay","direct","hour","old","by","admire","presentation","artificial","thesis","spite","hospital","project","recruit","discount"]
-;
+const words = ["correction", "harmony", "admission", "equal", "talkative", "twin", "camp", "middle", "candidate", "facade", "acid", "bronze", "reinforce", "tire", "sugar", "fragrant", "ballot", "basic", "pocket", "vigorous", "swear", "text", "coin", "copper", "rack", "small", "pain", "nest", "spit", "sea", "residence", "deter", "soar", "win", "or", "differ", "stay", "direct", "hour", "old", "by", "admire", "presentation", "artificial", "thesis", "spite", "hospital", "project", "recruit", "discount"]
+  ;
 
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -18,10 +18,11 @@ word.classList.add("word");
 
 function updateDisplay() {
   word.textContent = str;
-  container.textContent = "";
+  // container.textContent = "";
   container.appendChild(word);
 };
 
+let typeCount = 0;
 document.addEventListener('keydown', (event) => {
   if (event.key === str[0]) {
     str = str.slice(1, str.length)
@@ -29,8 +30,24 @@ document.addEventListener('keydown', (event) => {
       str += getRandomWord();
       str += " ";
     }
+    typeCount++;
   }
   updateDisplay();
 });
 
 updateDisplay();
+{
+  let time = document.createElement("div");
+  time.classList.add("time");
+  container.prepend(time);
+  console.log(time);
+}
+
+const startTime = new Date();
+
+const intervalId = setInterval(() => {
+  let time = document.querySelector(".time");
+  const elapsedTime = (new Date() - startTime) / 1000;
+  const wpm = (typeCount / 5) / (elapsedTime / 60);
+  time.textContent = `time:${elapsedTime.toFixed(1)}\nwpm:${wpm.toFixed(2)}`;
+}, 100);
